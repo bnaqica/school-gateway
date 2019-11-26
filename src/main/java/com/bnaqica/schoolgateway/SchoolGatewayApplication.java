7be8b3009648c420da1fce2ca0b59a96e9fbec4d
Brian Naqica <bnaqica@yahoo.com>
2019-11-25 22:58:44 -0700

    Skeleton of project commit

---
+++ src/main/java/com/bnaqica/schoolgateway/SchoolGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnaqica/schoolgateway/SchoolGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
